d7d57153e3adffb5e4d4e543b0e5ebf5a25e5eab
priyanka.m <priyanka.m@ojt6-PC.qedge.com>
2019-12-26 10:46:20 +0530

    ExcelOperations updated with nothing

---
+++ src/Readdatafromfile.java
@@ -0,0 +1,21 @@
+	import java.io.BufferedReader;
+	import java.io.FileReader;
+
+	public class Readdatafromfile {
+		public static void main(String[] args) throws Throwable{
+			FileReader fr=new FileReader("D://Selenium.txt");
+			BufferedReader br=new BufferedReader(fr);
+		String str=br.readLine();
+		/*while((str=br.readLine())!=null)
+		{
+		System.out.print(str);	
+		}*/
+		for(int i=0;i<str.length();i++)
+		{
+			System.out.println(str.charAt(i));
+		}
+		}
+	}
+
+	
+
